de8a846f73f81d190f80084851bfeda081d14720
bogdanGR <fant1k450@gmail.com>
2023-10-28 20:22:55 +0300

    Create JobApplication model, migration and factory, set relations in Job and User models

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Query\Builder as QueryBuilder;
 
 class Job extends Model
@@ -44,5 +45,10 @@ class Job extends Model
     public function employer(): BelongsTo
     {
         return $this->belongsTo(Employer::class);
+    }
+
+    public function jobApplications(): HasMany
+    {
+        return $this->hasMany(Employer::class);
     }
 }

2f9a6f55a9bfed2cc50c6af91813b1f43335ceb6
bogdanGR <fant1k450@gmail.com>
2023-11-08 08:21:37 +0200

    Job Policy

--- app/Http/Controllers/MyJobController.php
+++ app/Http/Controllers/MyJobController.php
@@ -13,6 +13,7 @@ class MyJobController extends Controller
      */
     public function index()
     {
+        $this->authorize('viewAnyEmployer', Job::class);
         return view('my_job.index',
             [
                 'jobs' => auth()->user()->employer->jobs()->with(
@@ -27,6 +28,7 @@ class MyJobController extends Controller
      */
     public function create()
     {
+        $this->authorize('create', Job::class);
         return view('my_job.create');
     }
 
@@ -35,7 +37,7 @@ class MyJobController extends Controller
      */
     public function store(JobRequest $request)
     {
-
+        $this->authorize('create', Job::class);
         auth()->user()->employer->jobs()->create($request->validated());
 
         return redirect()->route('my-jobs.index')
@@ -55,6 +57,7 @@ class MyJobController extends Controller
      */
     public function edit(Job $myJob)
     {
+        $this->authorize('update', $myJob);
         return view('my_job.edit', ['job' => $myJob]);
     }
 
@@ -63,6 +66,7 @@ class MyJobController extends Controller
      */
     public function update(JobRequest $request, Job $myJob)
     {
+        $this->authorize('update', $myJob);
         $myJob->update($request->validated());
 
         return redirect()->route('my-jobs.index')
